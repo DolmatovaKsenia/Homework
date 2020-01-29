dac618276b26af009922f4e9431ed1f6327f3f94
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-01-29 16:26:30 +0300

    added homework

---
+++ hw_28.01.20/z1.pas
@@ -0,0 +1,35 @@
+﻿//array 14
+
+const
+  size = 10;
+  
+var
+  arr: array [1..size] of integer;
+ 
+  
+  begin
+  for var i := 1 to size do
+    arr[i] := random(-9, 9);
+  
+  
+  for var i := 1 to size do
+    write(arr[i], ' ');
+  
+ 
+  var i:=0;
+  while (i <= size) do
+  begin
+  i := i + 2;
+  writeln(arr[i]);
+  end;
+ 
+  i:=1;
+  while (i <= size) do
+  begin
+  writeln(arr[i],' ');
+  i:=i+2;
+  end;
+end.
+
+
+НЕ ПОЛУЧИЛОСЬ
