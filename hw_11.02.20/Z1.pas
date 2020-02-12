ddf7846e75c2a2fb164bb8abb450afb67e17d907
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-02-12 16:05:28 +0300

    added homework

---
+++ hw_11.02.20/Z1.pas
@@ -0,0 +1,24 @@
+﻿// Дано вещественное число B, целое число N и набор из N вещественных чисел, упорядоченных по возрастанию. Вывести элементы набора
+// вместе с числом B, сохраняя упорядоченность выводимых чисел.
+
+var 
+  B, number: real;
+  N: integer;
+  first: boolean;
+  
+begin
+  readln(B);
+  readln(N);
+  first := false;
+  for var i := 1 to N do
+    begin
+    readln(number);
+    if (B <= number) and (first = false) then
+    begin
+      writeln(B);
+      first := true;
+      end;
+    end;
+  if first = false then
+    write(B) 
+end.
