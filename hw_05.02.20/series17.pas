0b918ab41fc43e0f3f214363fd9a3e9b2a730292
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-02-12 19:37:41 +0300

    added Z1

---
+++ hw_05.02.20/series17.pas
@@ -0,0 +1,27 @@
+﻿//series 17
+
+
+var
+  B: real;
+  N, number, c: integer;
+  
+begin
+  readln(B);
+  readln(N);
+  c := 0;
+  for var i := 1 to N do
+    begin
+    readln(number);
+  if (B < number) then
+  begin
+    writeln(B);
+    writeln(number)
+  end;
+  
+  if B > number then
+  begin
+  writeln(number);
+  writeln(B);
+  end;
+  end;
+end.
