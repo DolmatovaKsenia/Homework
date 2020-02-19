b54169d4efaed82bf1862fd520f9984de40c903e
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-02-19 16:34:59 +0300

    added homework (part 1)

---
+++ февраль/hw_16.02.20/array 31.pas
@@ -0,0 +1,28 @@
+﻿// array 31
+
+const
+  size = 10;
+  
+var
+  arr: array[1..size] of integer;
+  leftNeighbour, count: integer;
+  
+begin
+  count := 0;
+  for var i := 1 to size do
+  begin
+    arr[i] := random(-10, 10);
+    
+    leftNeighbour := arr[1];
+    
+    if arr[i] > leftNeighbour then
+    begin
+      write(arr[i]);
+      write(' ');
+      leftNeighbour := arr[i];
+      count := count + 1;
+      end;
+  end;
+  writeln(arr);
+  writeln(count)
+end.
---
+++ февраль/hw_16.02.20/array22.pas
@@ -0,0 +1,27 @@
+﻿const
+  size = 10;
+
+var
+  arr: array [1..size] of integer;
+  K, L, count, sum: integer;
+  
+begin
+  readln(K);
+  readln(L);
+  count := 0;
+  sum := 0;
+  for var i := 1 to size do
+  begin 
+    arr[i] := random(-10, 10);
+    writeln(arr[i], ' ');
+    
+  while i < K do
+    sum := sum + arr[i];
+    count := count + 1;
+    
+  while L + 1 < size do
+    sum := sum + arr[i];
+  end;
+  writeln(arr);
+  writeln('sum = ',sum)
+end.
