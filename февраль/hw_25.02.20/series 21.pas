81adda4d36b89cc873d57cfa6fceab553502b5df
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-02-26 16:35:01 +0300

    added homework

---
+++ февраль/hw_25.02.20/series 21.pas
@@ -0,0 +1,35 @@
+﻿// Series21°.
+// Дано целое число N (> 1) и набор из N вещественных чисел.
+// Проверить, образует ли данный набор возрастающую последовательность.
+// Если образует, то вывести True, если нет — вывести False.
+
+var
+  N: integer;
+  number, number1, lastNumber: real;
+  expressionAboutSequence: boolean;
+  
+begin
+  readln(N);
+  expressionAboutSequence := true;
+  readln(number1);
+  for var i := 2 to N do
+  begin
+    readln(number);
+    
+    if number1 < number then
+      expressionAboutSequence := true
+    else
+      begin
+      expressionAboutSequence := false;
+      lastNumber := number;
+      end;
+      
+    readln(number);
+    
+    if (lastNumber < number) and (expressionAboutSequence = true) then
+      expressionAboutSequence := true
+    else
+      expressionAboutSequence := false
+  end;
+writeln(expressionAboutSequence)
+end.
---
+++ февраль/hw_25.02.20/series20.pas
@@ -0,0 +1,25 @@
+﻿// Series20.
+// Дано целое число N (> 1) и набор из N целых чисел. 
+// Вывести те элементы в наборе, которые меньше своего правого соседа, и количество K таких элементов.
+
+var 
+  N, number, rightNeighbour, K, numberBefore: integer;
+  
+begin
+  readln(N);
+  readln(number);
+  numberBefore := number;
+  K := 0;
+  for var i := 2 to N do
+  begin
+    readln(number);
+    if numberBefore < number then
+    begin
+      rightNeighbour := numberBefore;
+      numberBefore := number;
+      K += 1;
+      writeln(rightNeighbour);
+    end;
+  end;
+  writeln(K)
+end.
