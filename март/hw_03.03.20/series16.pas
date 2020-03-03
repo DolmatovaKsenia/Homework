26f38fcb4cd9f6aaecfa0436715485610b5ebcf5
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-04 00:28:52 +0300

    added homework

---
+++ март/hw_03.03.20/series16.pas
@@ -0,0 +1,17 @@
+﻿// Series16°. Дано целое число K и набор ненулевых целых чисел; признак его завершения — число 0.
+// Вывести номер последнего числа в наборе, большего K. Если таких чисел нет, то вывести 0.
+
+var
+  number, K, count: integer
+  
+begin
+  readln(K);
+  count := 0;
+  readln(number);
+  while number <> 0 do
+  begin
+    if number > K then
+      count += 1;
+  end;
+  writeln(count)
+end.
