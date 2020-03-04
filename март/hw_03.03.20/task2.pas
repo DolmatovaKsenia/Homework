d2d02c9a468d50b1ca31e223896d6de49a83b62c
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-04 15:54:57 +0300

    added task2

---
+++ март/hw_03.03.20/task2.pas
@@ -0,0 +1,21 @@
+﻿// Требуется написать программу, которая по заданным натуральным числам a и b, а
+// также целому неотрицательному числу k, определяет k-плотность множества натуральных
+// чисел от a до b, включительно.
+
+var
+  a, b, k, count, x, y: integer;
+  
+begin
+  readln(a);
+  readln(b);
+  readln(k);
+  count := 0;
+  for var i := a to b do
+  begin
+  x := random(a, b);
+  y := random(a, b);
+  if abs(x * x - y * y * y) <= k then
+    count += 1;
+  end;
+  writeln(count)
+end.
