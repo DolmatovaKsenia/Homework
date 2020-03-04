9f2bd9eef79e83aba637b2e489be320f6c940b23
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-04 21:18:25 +0300

    fixed task2

--- март/hw_03.03.20/task2.pas
+++ март/hw_03.03.20/task2.pas
@@ -3,7 +3,7 @@
 // чисел от a до b, включительно.
 
 var
-  a, b, k, x1, x2, y1, y2: integer;
+  a, b, k, x1, x2, y1, y2, count: integer;
   
 begin
   readln(a, b, k);
@@ -12,10 +12,33 @@ begin
   y1 := 1;
   x2 := 1;
   y2 := 1;
-  
+  count := 0;
   while x1 * x1 < a do
   begin
     x1 += 1;
   end;
-  
+ while x2 * x2 < b do
+ begin
+   x2 += 1;
+ end;
+ x2 -= 1;
+ while y1 * y1 < a do
+ begin
+     y1 += 1;
+ end;
+ while y2 * y2 < b do
+ begin
+   y2 += 1;
+ end;
+ y2 -= 1;
+ for var x := x1 to x2 do
+ begin
+   for var y := y1 to y2 do
+   begin
+     if abs(x * x - y * y * y) <= k then
+       count += 1;
+   end;
+   
+ end;
+  writeln(count) 
 end.
