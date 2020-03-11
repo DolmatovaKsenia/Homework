f04efdd53c1d65ae11b4240f151ad031be1ab99f
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-11 16:34:51 +0300

    added hw

---
+++ март/hw_11.03.20/array49.pas
@@ -0,0 +1,25 @@
+﻿// array 49
+const
+  size = 5;
+
+var
+  arr: array[1..size] of integer;
+  count, prev: integer; 
+  
+begin 
+  count := 0;
+  for var i := 1 to size do
+  begin
+    readln(arr[i]);
+    count += 1;
+    prev := arr[i];
+    readln(arr[i]);
+      if arr[i] - prev <> 1 then
+      begin
+        writeln('Элемент нарушивший последлвательность ');
+        write(count)
+      end;
+     if not arr[i] - prev = 1 then 
+       writeln('0');
+  end;
+end.
---
+++ март/hw_11.03.20/array50.pas
@@ -0,0 +1,21 @@
+﻿// array 50
+const
+  size = 5;
+  
+var
+  arr: array[1..size] of integer;
+  count, prev: integer;
+  
+begin
+  count := 0;
+  readln(prev);
+  for var i := 2 to size do
+  begin
+    readln(arr[i]);
+    if prev > arr[i] then
+      count += 1;
+    prev := arr[i];
+  end;
+  writeln(count)
+end.
+  
