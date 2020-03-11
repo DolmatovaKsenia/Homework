e4f12db8c7b709c07424508d6fcd9afa31a34497
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-11 20:11:48 +0300

    Create array49(variant 2).pas

---
+++ март/hw_11.03.20/array49(variant 2).pas
@@ -0,0 +1,32 @@
+﻿// array 49
+const
+  size = 5;
+
+var
+  arr1: array[1..size] of integer;
+  arr2: array[1..size] of integer;
+  count: integer;
+  
+begin
+  count := 0;
+  for var i := 1 to size do
+    readln(arr1[i]);
+  for var i := 1 to size do
+    arr2[i] := 0; 
+    
+  for var i := 1 to size do
+  begin
+    if arr2[i] = 0 then
+    begin
+      arr2[i] += 1;
+      count += 1;
+    end;
+    if arr2[i] = 1 then
+    begin
+      writeln(count);
+      break
+    end
+    else
+      writeln('0');
+    end;
+end.
