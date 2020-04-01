d6f074518e16d4b5ae810f923cf57d591113808c
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-04-01 12:46:00 +0300

    added homework

---
+++ март/hw_31.03.20/matrix15.pas
@@ -0,0 +1,46 @@
+﻿// matrix 15
+
+const
+  rows = 5;
+  columns = 5;
+  
+var
+  arr: array[1..rows, 1..columns] of int64;
+  i1, jCol, iRow: integer;
+begin
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+    begin
+      arr[i, j] := random(10, 30);
+    end;
+  end;
+  
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+    begin
+      write(arr[i, j], ' ');
+    end;
+  end;
+  
+  writeln();
+  
+  i1 := 1;
+  for var j := 1 to columns do
+  begin
+    write(arr[i1, j], ' ');
+  end;
+  
+  jCol := 5;
+  for var i := 2 to rows do
+  begin
+    write(arr[i, jCol], ' ');
+  end;
+  
+  iRow := 5;
+  for var j := 1 downto columns - 1 do
+  begin
+    write(arr[iRow, j], ' ');
+  end;
+end.
